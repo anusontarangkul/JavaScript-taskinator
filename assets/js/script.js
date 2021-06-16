b74bb32063f4d0913de3984e6f075c48abda24d0
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-06-16 00:00:04 -0700

    add delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function () {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -97,4 +98,18 @@ var createTaskActions = function (taskId) {
     return actionContainerEl;
 }
 
+var taskButtonHandler = function (event) {
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+}
+
+var deleteTask = function (taskId) {
+    var taskSlected = document.querySelector(".task-item[data-task-id='" + taskId + "']")
+    taskSlected.remove();
+}
+
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
